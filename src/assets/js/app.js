bf6b29fa0408ec70c6db6802de413a99734313e0
Damià Arnau Pont <damiarnau@gmail.com>
2017-11-05 19:38:52 +0100

    tipos i ajustos menors

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,8 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
